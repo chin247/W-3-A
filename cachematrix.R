7afaec0ed3e0adc288b1b4ba0fa7a4f6fa32b1e2
chin247 <coolchin01@yahoo.com>
2024-03-10 20:38:28 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+makeCacheMatrix <- function(x = matrix(), t) {
+     
+     # set.seed number for reproducibility
+     set.seed(t)       
+     
+      # Generate the random matrix with x representing the # squared of observation 
+      # and columns
+       mtx<-matrix(rnorm(x^2), ncol = x)
+
+      # return the cache
+     return(mtx)
+
 }
+
+imx<-makeCacheMatrix(3, 78) # a stored matrix object 
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x) { ## Return a matrix that is the inverse of 'x'
+    
+     tryCatch({ # The tryCatch() captures any errors that occur if the 
+          # the inverse of the matrix can not be computed by solve(), 
+          inv<-solve(x) # computes and returns the inverse of the x = matrix
+          return(inv)
+     }, error = function(e) { # the error handling function that returns "Error" if the solve() incounters an error
+          return("Error: Invalid matrix to compute an inverse")  
+          
+     })
+     
+}
+
+
+cacheSolve(imx) # produce an inverse of the matrix created by makeCacheMatrix (x,t)
